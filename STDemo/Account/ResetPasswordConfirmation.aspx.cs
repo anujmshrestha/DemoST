4aa9850d8540663b292ab2b9c2d0bcd1c08f3918
anujmshrestha <anujshrestha15@gmail.com>
2017-05-07 13:40:03 +0545

    first commit

---
+++ STDemo/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace STDemo.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ STDemo/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(STDemo.Startup))]
+namespace STDemo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
